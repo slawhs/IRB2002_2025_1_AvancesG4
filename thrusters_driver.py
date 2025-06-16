8b75b502e44e586c7348a58c992ad672dd699c72
slawhs <gajardo.ignacio@uc.cl>
2025-06-15 23:01:12 -0400

    Anexo de códigos Avance 2

---
+++ thrusters_driver.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+from chungungo_interfaces.srv import VelocityCommand
+
+from serial import Serial
+import time
+
+DEVICE_NAME = '/ttyCH341USB0'
+BAUD_RATE = 115200
+
+class Thrusters(Node):
+    def __init__(self):
+        super().__init__("Thrusters")
+
+        # -------- Publishers, Subscribers Clients and Services --------
+        self.vel_cmd_srv = self.create_service(VelocityCommand, '/vel_command', self.vel_command_cb)
+
+        # -------- Atributes --------
+        self.serial = None
+
+        # -------- Setup Routines --------
+        self.serial_setup()
+        self.get_logger().info("Setup compelted")
+
+    def serial_setup(self):
+        self.serial = Serial('/dev' + DEVICE_NAME, BAUD_RATE)
+        if not self.serial.isOpen():
+            self.get_logger().warning('Serial port is not open yet')
+        self.get_logger().info('Serial port connected')
+
+    def vel_command_cb(self, request, response):
+        vel_cmd = f"{request.left_velocity},{request.right_velocity}\n"
+        self.get_logger().info("==================== VELOCITY COMMAND RECIEVED ====================")
+        self.get_logger().info(f"Left setpoint:{request.left_velocity} rpm")
+        self.get_logger().info(f"Right setpoint:{request.right_velocity} rpm")
+
+        self.serial.write(vel_cmd.encode("utf-8"))
+        response.left_rpm, response.right_rpm = self.get_rpm_feedback()
+
+        return response
+
+    def get_rpm_feedback(self):
+        self.get_logger().info("Getting feedback...")
+        rpm_feedback = self.serial.readline().decode('ascii', errors='replace')
+        self.get_logger().info("Feedback recieved")
+
+        left_rpm, right_rpm = rpm_feedback.strip().split("|")
+        left_rpm = int(round(float(left_rpm)))
+        right_rpm = int(round(float(right_rpm)))
+
+        self.get_logger().info(f"Left: {left_rpm} RPM | Right: {right_rpm} RPM")
+
+        return left_rpm, right_rpm
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Thrusters()
+    rclpy.spin(node)
+    
+    node.serial.close()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
---
+++ thrusters_velocity_mux.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+from chungungo_interfaces.srv import VelocityCommand
+from geometry_msgs.msg import Vector3  #? Do custom interface for joystick_speed
+
+class ThrustersVelMux(Node):
+    def __init__(self):
+        super().__init__("ThrustersVelocityMux")
+
+        # -------- Publishers, Subscribers and Services --------
+        #* crear subscriber de nodos de teleop, control de boyas, control de distancias. 
+        self.joystick_vel_sub = self.create_subscription(Vector3, '/joystick_velocity', self.joystick_vel_cb, 10)
+
+        self.vel_cmd_cli = self.create_client(VelocityCommand, '/vel_command')
+        while not self.vel_cmd_cli.wait_for_service(timeout_sec=0.5):
+            self.get_logger().info('Service not available, waiting again...')
+
+        # -------- Atributes --------
+        timer_period = 0.1  # Segundos
+        self.vel_mux_timer = self.create_timer(timer_period, self.vel_mux_cb)
+        self.request = VelocityCommand.Request()
+        
+        self.joystick_left_vel = 0.0
+        self.joystick_right_vel = 0.0
+
+        # -------- Setup Routines --------
+
+    def joystick_vel_cb(self, msg):
+        self.joystick_left_vel = msg.x
+        self.joystick_right_vel = msg.y
+
+    def vel_mux_cb(self):
+        # Joystick velocity
+        self.request.left_velocity = self.joystick_left_vel
+        self.request.right_velocity = self.joystick_right_vel
+        self.future = self.vel_cmd_cli.call_async(self.request)
+
+        if self.future.done():
+            try:
+                response = self.future.result()
+            except Exception as e:
+                self.get_logger().info(f"Service call failed {e}")
+            else:
+                self.get_logger().info(f"RPM Left = {response.left_rpm}")
+                self.get_logger().info(f"RPM Right = {response.right_rpm}")
+   
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ThrustersVelMux()
+    rclpy.spin(node)
+    node.destroy_node()
+
+
+if __name__ == "__main__":
+    main()
